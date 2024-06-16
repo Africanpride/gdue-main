a2ee26cec6aee6f087646e5612f8672059e5f8d1
Pius Opoku-Fofie <pius1919@gmail.com>
2024-06-16 00:02:16 +0000

    need more content ..2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,22 @@
+import Jumbotron from '@/components/Jumbotron'
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>Welcome to Dashboard</div>
+    <div className="md:space-y-16">
+
+      <Jumbotron
+        heading="Manage Your Business Details"
+        description={(<>  Welcome to your GDUE Dashboard! Here, you can easily add and manage your business details to be featured in our Diasporan Business Directory. Connect with the community, enhance your visibility, and grow your network. Start by entering your business information and join a vibrant network of Ghanaian entrepreneurs
+        </>)}
+        linkText="Watch our story"
+        linkHref="#"
+        backgroundImage="/images/cargo2.jpg"
+        hideVideo={false}
+      />
+      </div>
   )
 }
 
