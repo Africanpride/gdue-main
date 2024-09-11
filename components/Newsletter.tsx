844021f58f981ac2239b7706ca097a2706f68794
Pius Opoku-Fofie <pius1919@gmail.com>
2024-09-11 21:41:12 +0000

    updated Newsletter ..

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -11,21 +11,21 @@ const Newsletter = (props: Props) => {
       <AnimatedSections className="w-full text-[16px] ">
         <div className="lg:max-w-none  mt-22 mx-auto sm:mt-30">
           <div
-            className="-mx-6 md:rounded-6xl bg-neutral-950 px-6 py-12 sm:mx-0 sm:py-24 md:px-12"
+            className="-mx-6 md:rounded-6xl bg-secondary px-6 py-12 sm:mx-0 sm:py-24 md:px-12"
             style={{ opacity: "1", transform: "none" }}
           >
-            <div className="mx-auto max-w-4xl divide-y divide-neutral-900 space-y-8  ">
+            <div className="mx-auto max-w-4xl divide-y divide-neutral-300/25 space-y-8  ">
 
               <div className="max-w-2xl space-y-5 ">
                 <h2 className="font-display text-3xl  text-white [text-wrap:balance] sm:text-4xl">
                 Join the Ghanaian Diaspora Union in Europe (GDUE)!
                 </h2>
-                <p className="text-md text-gray-500">
+                <p className="text-md text-white">
                 Unite with fellow Ghanaians in Europe to build lasting friendships, share cultural experiences, and advance your career. Empower yourself with the GDUE network today!
                 </p>
                 <div className="mt-6 flex">
                   <Link
-                    className="inline-flex items-center rounded-full px-4 py-1.5 space-x-3 text-sm font-semibold transition bg-white text-neutral-950 hover:bg-neutral-200"
+                    className="inline-flex items-center rounded-full px-4 py-1.5 space-x-3 text-sm font-semibold transition bg-yellow-500 text-black hover:bg-yellow-400"
                     href="/join"
                   >
                     <span className="relative top-px">Click Here to Join GDUE</span>
@@ -46,8 +46,8 @@ const Newsletter = (props: Props) => {
                   className="mt-2 grid grid-cols-1 gap-8 sm:grid-cols-2"
                 >
                   <li>
-                    <address className="text-sm not-italic text-neutral-300">
-                      <strong className="text-white">Barcelona</strong><br />
+                    <address className="text-sm not-italic text-neutral-100">
+                      <strong className="text-white text-lg">&#x2014; Spain</strong><br />
                       Calle Río sella, 43 Bajo izquierda, 33010
                       <br />Oviedo Spain, +34 624490895
                     </address>
